3e64b4142211e3a2fc28cba97fde8e6a68cbdf0e
nidhi2793 <MY_NAME@example.com>
2023-09-25 12:44:03 +0530

    task-8 Get the messages from the Database

--- controller/userController.js
+++ controller/userController.js
@@ -5,11 +5,9 @@ const jwt = require("jsonwebtoken");
 const sequelize = require("../util/database");
 const Sib = require("sib-api-v3-sdk");
 const { Op } = require("sequelize");
-
 function generateAccessToken(id, email) {
   return jwt.sign({ userId: id, email: email }, process.env.TOKEN);
 }
-
 const getLoginPage = async (req, res, next) => {
   try {
     res.sendFile(path.join(__dirname, "../", "public", "views", "login.html"));
@@ -17,7 +15,6 @@ const getLoginPage = async (req, res, next) => {
     console.log(error);
   }
 };
-
 const postUserSignUp = async (req, res, next) => {
   try {
     const name = req.body.name;
@@ -25,39 +22,41 @@ const postUserSignUp = async (req, res, next) => {
     const number = req.body.number;
     const password = req.body.password;
 
-    await User.findOne({ [Op.or]: [{ email: email }, { number: number }] })
-      .then((user) => {
-        if (user) {
-          res
-            .status(409)
-            .send(
-              `<script>alert('This email or number is already taken. Please choose another one.'); window.location.href='/'</script>`
-            );
-        } else {
-          bcrypt.hash(password, 10, async (err, hash) => {
-            await User.create({
-              name: name,
-              email: email,
-              number: number,
-              password: hash,
-            });
-          });
-          res
-            .status(200)
-            .send(
-              `<script>alert('Account Created Successfully!'); window.location.href='/'</script>`
-            );
-        }
-      })
-      .catch((err) => console.log(err));
-  } catch (error) {}
-};
+    const existingUser = await User.findOne({
+      where: {
+        [Op.or]: [{ email }, { number }],
+      },
+    });
 
+    if (existingUser) {
+      res
+        .status(409)
+        .send(
+          `<script>alert('This email or number is already taken. Please choose another one.'); window.location.href='/'</script>`
+        );
+    } else {
+      bcrypt.hash(password, 10, async (err, hash) => {
+        await User.create({
+          name: name,
+          email: email,
+          number: number,
+          password: hash,
+        });
+      });
+      res
+        .status(200)
+        .send(
+          `<script>alert('User Created Successfully!'); window.location.href='/'</script>`
+        );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 const postUserLogin = async (req, res, next) => {
   try {
     const email = req.body.loginEmail;
     const password = req.body.loginPassword;
-
     await User.findOne({ where: { email: email } }).then((user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, result) => {
@@ -90,7 +89,6 @@ const postUserLogin = async (req, res, next) => {
     console.log(error);
   }
 };
-
 module.exports = {
   getLoginPage,
   postUserSignUp,
